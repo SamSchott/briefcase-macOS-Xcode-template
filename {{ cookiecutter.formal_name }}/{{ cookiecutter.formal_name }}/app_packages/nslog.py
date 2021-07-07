c6a80d37272272036b35875c399ba194e103bf3e
Sam Schott <ss2151@cam.ac.uk>
2021-07-07 01:29:27 +0100

    remove rediction of stdout and stderr

--- {{ cookiecutter.formal_name }}/{{ cookiecutter.formal_name }}/app_packages/nslog.py
+++ {{ cookiecutter.formal_name }}/{{ cookiecutter.formal_name }}/app_packages/nslog.py
@@ -12,85 +12,11 @@
 # onto the `sys` module as `sys._traceback`; this can be retrieved by the
 # C API and used.
 
-import ctypes
 import io
 import re
 import sys
 import traceback
 
-# Name of the UTF-16 encoding with the system byte order.
-if sys.byteorder == 'little':
-    UTF16_NATIVE = 'utf-16-le'
-elif sys.byteorder == 'big':
-    UTF16_NATIVE = 'utf-16-be'
-else:
-    raise AssertionError('Unknown byte order: ' + sys.byteorder)
-
-
-class CFTypeRef(ctypes.c_void_p):
-    pass
-
-CFIndex = ctypes.c_long
-UniChar = ctypes.c_uint16
-
-CoreFoundation = ctypes.CDLL('/System/Library/Frameworks/CoreFoundation.framework/CoreFoundation')
-Foundation = ctypes.CDLL('/System/Library/Frameworks/Foundation.framework/Foundation')
-
-# void CFRelease(CFTypeRef arg)
-CoreFoundation.CFRelease.restype = None
-CoreFoundation.CFRelease.argtypes = [CFTypeRef]
-
-# CFStringRef CFStringCreateWithCharacters(CFAllocatorRef alloc, const UniChar *chars, CFIndex numChars)
-CoreFoundation.CFStringCreateWithCharacters.restype = CFTypeRef
-CoreFoundation.CFStringCreateWithCharacters.argtypes = [CFTypeRef, ctypes.POINTER(UniChar), CFIndex]
-
-# void NSLog(NSString *format, ...)
-Foundation.NSLog.restype = None
-Foundation.NSLog.argtypes = [CFTypeRef]
-
-
-def _cfstr(s):
-    """Create a ``CFString`` from the given Python :class:`str`."""
-    encoded = s.encode(UTF16_NATIVE)
-    assert len(encoded) % 2 == 0
-    arr = (UniChar * (len(encoded)//2)).from_buffer_copy(encoded)
-    cfstring = CoreFoundation.CFStringCreateWithCharacters(None, arr, len(arr))
-    assert cfstring is not None
-    return cfstring
-
-
-# Format string for a single object.
-FORMAT = _cfstr('%@')
-
-
-def nslog(s):
-    """Log the given Python :class:`str` to the system log."""
-    cfstring = _cfstr(s)
-    Foundation.NSLog(FORMAT, cfstring)
-    CoreFoundation.CFRelease(cfstring)
-
-
-class NSLogWriter(io.TextIOBase):
-    """An output-only text stream that writes to the system log."""
-    def write(self, s):
-        if not hasattr(self, 'buf'):
-            self.buf = s
-        else:
-            self.buf += s
-
-        lines = self.buf.split('\n')
-        for line in lines[:-1]:
-            nslog(line)
-        self.buf = lines[-1]
-        return len(s)
-
-    @property
-    def encoding(self):
-        return UTF16_NATIVE
-
-
-# Replace stdout and stderr with a single NSLogWriter
-sys.stdout = sys.stderr = NSLogWriter()
 
 # Install a custom exception hook.
 def custom_exception_hook(exc_type, value, tb):
